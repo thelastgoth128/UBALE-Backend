5a89077b0efc63a81ffd1ef3558e5dd17729a70e
thelastgoth128 <thindwacyrus86@gmail.com>
2025-06-06 21:23:56 +0200

    setting up user entity

--- src/components/user/entities/user.entity.ts
+++ src/components/user/entities/user.entity.ts
@@ -1,2 +1,59 @@
-@Entity
-export class User {}
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class User {
+    @PrimaryGeneratedColumn()
+    userid : number
+
+    @Column()
+    name : string
+
+    @Column()
+    email : string
+
+    @Column()
+    password : string
+
+    @Column()
+    location : string
+
+    @Column()
+    age : number
+
+    @Column()
+    bio : string
+
+    @Column()
+    distance : string
+
+    @Column()
+    education : string
+
+    @Column()
+    gender : string
+
+    @Column()
+    height : string
+
+    @Column()
+    hobbies : string
+
+    @Column()
+    languages : string
+
+    @Column()
+    interest : string
+
+    @Column()
+    lifesytle : string
+
+    @Column()
+    occupation : string
+
+    @Column()
+    phone : number
+
+    @Column()
+    profilepic : string
+
+}
