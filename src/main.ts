09ff410e0d7ecd15196f6795cefa6e92b18b25f6
thelastgoth128 <thindwacyrus86@gmail.com>
2025-06-09 20:43:34 +0200

    made firebase authontication global

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
-import { NestFactory } from '@nestjs/core';
+import { NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { AuthGuard } from './components/auth/guards/auth.guard';
+import { FirebaseService } from './components/services/firebase.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const firebaseService = app.get(FirebaseService);
+  const reflector = app.get(Reflector);
+
+  app.useGlobalGuards(new AuthGuard(firebaseService, reflector));
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
