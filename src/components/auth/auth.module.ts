35b95e84c9cb25387f421997b3900418f700e0f1
thelastgoth128 <thindwacyrus86@gmail.com>
2025-06-09 20:44:30 +0200

    configured firebase in authguard

--- src/components/auth/auth.module.ts
+++ src/components/auth/auth.module.ts
@@ -1,9 +1,15 @@
-import { Module } from '@nestjs/common';
+import { forwardRef,Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { UserModule } from '../user/user.module';
+import { FirebaseService } from '../services/firebase.service';
 
 @Module({
+  imports:[
+    forwardRef(()=>UserModule),
+  ],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService,FirebaseService],
+  exports:[FirebaseService]
 })
 export class AuthModule {}
--- src/components/user/user.module.ts
+++ src/components/user/user.module.ts
@@ -3,12 +3,15 @@ import { UserService } from './user.service';
 import { UserController } from './user.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
+import { AuthModule } from '../auth/auth.module';
+
 
 @Module({
   imports:[
-    TypeOrmModule.forFeature([User]),
+    TypeOrmModule.forFeature([User]),AuthModule
   ],
   controllers: [UserController],
   providers: [UserService],
+  exports: [TypeOrmModule,UserService]
 })
 export class UserModule {}
