8a7609523fd655c6df15db6d9a34dac87d3ad2c3
thelastgoth128 <thindwacyrus86@gmail.com>
2025-06-14 18:21:14 +0200

    used mailservice as a provide

--- src/components/auth/auth.module.ts
+++ src/components/auth/auth.module.ts
@@ -3,13 +3,14 @@ import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { UserModule } from '../user/user.module';
 import { FirebaseService } from '../services/firebase.service';
+import { MailService } from '../services/mail.service';
 
 @Module({
   imports:[
     forwardRef(()=>UserModule),
   ],
   controllers: [AuthController],
-  providers: [AuthService,FirebaseService],
+  providers: [AuthService,FirebaseService,MailService],
   exports:[FirebaseService]
 })
 export class AuthModule {}
